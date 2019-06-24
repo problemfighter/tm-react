f57b87199e6357b09270cd2d57df0f236eceb0ed
Touhid Mia <touhid@bitmascot.com>
2019-06-24 19:16:44 +0600

    Axios Implementation

--- src/artifacts/processor/axios/axios-http-manager.ts
+++ src/artifacts/processor/axios/axios-http-manager.ts
@@ -1,14 +1,19 @@
 import TRHTTPManager from "../http/tr-http-manager";
 import TRHTTRequest from "../http/tr-http-request";
 import TRHTTCallback from "../http/tr-http-callback";
-import axios from 'axios';
+import axios, {AxiosError, AxiosResponse} from 'axios';
 
 export default class AxiosHTTPManager implements TRHTTPManager{
 
 
+    private httpCall() {
+        axios.get('/user?ID=12345').then((response: AxiosResponse) => {
 
-    private httpCall(){
-        axios.get('/user?ID=12345');
+        }).catch((error: AxiosError) => {
+
+        }).finally(() => {
+
+        });
     }
 
    public delete(request: TRHTTRequest, callback: TRHTTCallback): void {
