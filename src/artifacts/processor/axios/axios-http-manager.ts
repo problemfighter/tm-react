9d37e91c728d6303f5eeda13904873165817bf3d
Touhid Mia <touhid@bitmascot.com>
2019-06-24 18:39:59 +0600

    Axios Implementation

---
+++ src/artifacts/processor/axios/axios-http-manager.ts
@@ -0,0 +1,30 @@
+import TRHTTPManager from "../http/tr-http-manager";
+import TRHTTRequest from "../http/tr-http-request";
+import TRHTTCallback from "../http/tr-http-callback";
+
+export default class AxiosHTTPManager implements TRHTTPManager{
+
+
+    delete(request: TRHTTRequest, callback: TRHTTCallback): void {
+    }
+
+
+    deleteJSON(request: TRHTTRequest, callback: TRHTTCallback): void {
+    }
+
+
+    get(request: TRHTTRequest, callback: TRHTTCallback): void {
+    }
+
+
+    post(request: TRHTTRequest, callback: TRHTTCallback): void {
+    }
+
+
+    postJSON(request: TRHTTRequest, callback: TRHTTCallback): void {
+    }
+    
+    
+
+    
+}
