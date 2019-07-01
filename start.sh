d6eb10eeaea9002dbe6cc76da2999b4519fde4e8
Touhid Mia <touhid@bitmascot.com>
2019-07-01 11:54:07 +0600

    Install Instruction

--- start.sh
+++ start.sh
@@ -1,15 +1,25 @@
+PROJECT_NAME="tm-react"
 echo "Welcome to TM React Project";
 echo "-------------------------------------------------------------"
 echo "-------------------------------------------------------------"
+
 echo "Cloning Root Project From GitHub";
+if [ -d "$PROJECT_NAME" ]; then
+  rm -rf "$PROJECT_NAME"
+fi
 git clone https://github.com/hmtmcse/tm-react.git
-cd 
+
+
+cd "$PROJECT_NAME"
 echo "Cloning Project Wiki From GitHub";
+if [ -d "tm-react.wiki" ]; then
+  rm -rf tm-react.wiki
+fi
 git clone https://github.com/hmtmcse/tm-react.wiki.git
 
 echo "Coping Samples";
 if [ -d "src/app" ]; then
-  rm -rf app
+  rm -rf src/app
 fi
 
 if [ -d "./sample/basic" ]; then
@@ -19,7 +29,6 @@ if [ -d "./sample/basic" ]; then
       exit 1
 fi
 
-cd ..
 echo "Installing yarn";
 npm install -g yarn
 
