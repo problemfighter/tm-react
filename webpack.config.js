bb80d8d35a73bffe48ed935ee0e620678cb4b208
Touhid Mia <touhid@bitmascot.com>
2019-07-07 14:48:36 +0600

    Added WebPack for Build

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const AsyncChunkNames = require('webpack-async-chunk-names-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/index.tsx'
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js', '.jsx' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        chunkFilename: '[name].js',
+        path: path.resolve(__dirname, 'build')
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+                vendor: {
+                    name: 'vendor',
+                    chunks: 'all',
+                    test: /node_modules/
+                }
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'TM React Application',
+            chunksSortMode: 'none',
+            template: './public/index.html',
+            favicon: './public/favicon.ico',
+            minify: true
+        }),
+        new AsyncChunkNames()
+    ],
+
+    devServer: {
+        port: 9000,
+        open:true
+    }
+};
