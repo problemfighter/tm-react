ec39f8abbca3dc428ebbe5c6ca6c121a02cfd88c
Touhid Mia <touhid@bitmascot.com>
2019-07-10 13:26:04 +0600

    Update Page Manager

--- src/artifacts/manager/tr-page-manager.tsx
+++ src/artifacts/manager/tr-page-manager.tsx
@@ -1,11 +1,9 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, {Suspense} from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import TRLayoutInfoData from '../data/view/tr-layout-info-data';
 import TRPageInfoData from '../data/view/tr-page-info-data';
 import {TRPageManagerProps, TRPageManagerState} from '../model/tr-model';
 import TRReactComponent from "../framework/tr-react-component";
-
-
 
 
 export default class TRPageManager extends TRReactComponent<TRPageManagerProps, TRPageManagerState> {
@@ -18,19 +16,19 @@ export default class TRPageManager extends TRReactComponent<TRPageManagerProps, 
                 key={index}
                 render={(route) => {
                     const Layout = trLayout;
-                    return (<React.Fragment><Layout component={pageInfoData.component} route={route} /></React.Fragment>)
+                    return (<React.Fragment><Layout component={pageInfoData.component} route={route}/></React.Fragment>)
                 }}
             />
         )
     }
 
 
-    private generateURL(pageInfoDataList: Array<TRPageInfoData>, layoutData: any, index: any){
-            return pageInfoDataList.map((pageInfoData: TRPageInfoData, nestedIndex) => {
-                if (pageInfoData.isActive) {
-                    return this.getRouter(pageInfoData, layoutData.layout, index);
-                }
-            });
+    private generateURL(pageInfoDataList: Array<TRPageInfoData>, layoutData: any, index: any) {
+        return pageInfoDataList.map((pageInfoData: TRPageInfoData, nestedIndex) => {
+            if (pageInfoData.isActive) {
+                return this.getRouter(pageInfoData, layoutData.layout, index);
+            }
+        });
     }
 
 
@@ -42,19 +40,18 @@ export default class TRPageManager extends TRReactComponent<TRPageManagerProps, 
                 <Suspense fallback={appConfig.getSuspenseLoader}>
                     <Switch>
                         {
-                            urlMapping.getLayoutsAndPages().map((layoutData: TRLayoutInfoData, index) => {
+                            urlMapping.getLayoutsAndPages().map((layoutData: TRLayoutInfoData, index: any) => {
                                 if (layoutData.pageInfoDataList.length !== 0) {
                                     return this.generateURL(layoutData.pageInfoDataList, layoutData, index);
                                 }
                             })
                         }
-                        <Route component={appConfig.getNotFoundView} />
+                        <Route component={appConfig.getNotFoundView}/>
                     </Switch>
                 </Suspense>
             </BrowserRouter>
         );
     }
-
 
 
 }
