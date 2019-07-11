bb18e145483308ac5223158a07b79588b9a6eb12
Touhid Mia <hmtm.cse@gmail.com>
2019-07-11 19:41:14 +0600

    TM React source

--- src/app/config/app-config.tsx
+++ src/app/config/app-config.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import TRAppConfig from "../../artifacts/config/tr-app-config";
-import TRHTTResponse from '../../artifacts/processor/http/tr-http-response';
 
 
 export default class AppConfig extends TRAppConfig {
