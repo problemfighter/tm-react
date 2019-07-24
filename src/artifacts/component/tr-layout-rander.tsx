4593ae2bfdb142630a3a7c2ced2ad341e4271ab4
Touhid Mia <touhid@bitmascot.com>
2019-07-24 09:10:09 +0600

    Suspense

--- src/artifacts/component/tr-layout-rander.tsx
+++ src/artifacts/component/tr-layout-rander.tsx
@@ -7,15 +7,17 @@ export interface LayoutRendererProps extends TRProps {
     route: any
     appConfig: any
     component: any
+    suspenseLoader?: any
 }
 
 export default class TRLayoutRenderer extends TRReactComponent<LayoutRendererProps, TRState> {
     render() {
         const Component = this.props.component;
-        const {route, appConfig} = this.props;
+        const {route, appConfig, suspenseLoader} = this.props;
+        const suspense = suspenseLoader ? suspenseLoader : appConfig.getSuspenseLoader();
         return (
             <React.Fragment>
-                <Suspense fallback={appConfig.getSuspenseLoader()}>
+                <Suspense fallback={suspense}>
                     <Component route={route} appConfig={appConfig}/>
                 </Suspense>
             </React.Fragment>
